7d21391c0289fd3c365a0f7a92b38eaafb2d4e48
Your Name <you@example.com>
2022-04-07 22:00:41 +0300

    сделал домашку

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,11 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+     System.out.println(27061985);
+
+
 	// write your code here
         System.out.println("hello world");  }
 }
